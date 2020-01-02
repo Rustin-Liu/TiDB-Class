0eefcb5b9ab3dad61fdbbb7fce6736fad4b26790
Rustin-Liu <rustin.liu@gmail.com>
2020-01-02 21:38:40 +0800

    feat(class1-mergesort): impl regular version

--- class1/mergesort.go
+++ class1/mergesort.go
@@ -3,4 +3,40 @@ package main
 // MergeSort performs the merge sort algorithm.
 // Please supplement this function to accomplish the home work.
 func MergeSort(src []int64) {
+	mergeSort(src, 0, int64(len(src)))
+}
+func mergeSort(src []int64, lo int64, hi int64) {
+	if hi-lo < 2 {
+		return
+	}
+	var mi = (lo + hi) / 2
+	mergeSort(src, lo, mi)
+	mergeSort(src, mi, hi)
+
+	merge(src, lo, mi, hi)
+}
+
+func merge(src []int64, lo int64, mi int64, hi int64) {
+	a := src[lo:]
+	var lb = mi - lo
+	b := make([]int64, lb)
+	var i, j, k int64
+	for i = 0; i < lb; i++ {
+		b[i] = a[i]
+	}
+	var lc = hi - mi
+	c := src[mi:]
+	for i, j, k = 0, 0, 0; (j < lb) || (k < lc); {
+		if (j < lb) && (!(k < lc) || (b[j] <= c[k])) {
+			a[i] = b[j]
+			i++
+			j++
+		}
+		if (k < lc) && (!(j < lb) || c[k] < b[j]) {
+			a[i] = c[k]
+			i++
+			k++
+		}
+	}
+
 }
