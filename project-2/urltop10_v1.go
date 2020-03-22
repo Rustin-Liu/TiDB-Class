0fdf85bbc057a34376be16a24017a8d655336e49
Rustin-Liu <rustin.liu@gmail.com>
2020-03-23 01:29:12 +0800

    refactor(mapreduce): improve map perf
    Signed-off-by: Rustin-Liu <rustin.liu@gmail.com>

--- project-2/urltop10_v1.go
+++ project-2/urltop10_v1.go
@@ -8,14 +8,14 @@ import (
 	"sync/atomic"
 )
 
-var count int64
+var countV1 int64
 
 // URLTop10 generates RoundsArgs for getting the 10 most frequent URLs.
 // There are two rounds in this approach.
 // The first round will do url count.
 // The second will sort results generated in the first round and
 // get the 10 most frequent URLs.
-func URLTop10(nWorkers int) RoundsArgs {
+func URLTop10V1(nWorkers int) RoundsArgs {
 	var args RoundsArgs
 	// round 1: do url count.
 	args = append(args, RoundArgs{
@@ -33,42 +33,26 @@ func URLTop10(nWorkers int) RoundsArgs {
 }
 
 // URLCountMap is the map function in the first round.
-func URLCountMap(filename string, contents string) []KeyValue {
+func URLCountMapV1(filename string, contents string) []KeyValue {
 	lines := strings.Split(contents, "\n")
 	kvs := make([]KeyValue, 0, len(lines))
-	temp := make(map[string]int)
 	for _, l := range lines {
 		l = strings.TrimSpace(l)
 		if len(l) == 0 {
 			continue
 		}
-		count, ok := temp[l]
-		if ok {
-			temp[l] = count + 1
-		} else {
-			temp[l] = 1
-		}
-	}
-	for k, v := range temp {
-		kvs = append(kvs, KeyValue{Key: k, Value: strconv.Itoa(v)})
+		kvs = append(kvs, KeyValue{Key: l})
 	}
 	return kvs
 }
 
 // URLCountReduce is the reduce function in the first round.
-func URLCountReduce(key string, values []string) string {
-	total := 0
-	for _, count := range values {
-		temp, err := strconv.Atoi(count)
-		if err == nil {
-			total += temp
-		}
-	}
-	return fmt.Sprintf("%s %s\n", strconv.Itoa(total), key)
+func URLCountReduceV1(key string, values []string) string {
+	return fmt.Sprintf("%s %s\n", strconv.Itoa(len(values)), key)
 }
 
 // URLTop10Map is the map function in the second round.
-func URLTop10Map(filename string, contents string) []KeyValue {
+func URLTop10MapV1(filename string, contents string) []KeyValue {
 	lines := strings.Split(contents, "\n")
 	kvs := make([]KeyValue, 0, len(lines))
 	for _, line := range lines {
@@ -79,18 +63,18 @@ func URLTop10Map(filename string, contents string) []KeyValue {
 		words := strings.Split(line, " ")
 		kvs = append(kvs, KeyValue{words[0], words[1]})
 	}
-	atomic.StoreInt64(&count, 0)
+	atomic.StoreInt64(&countV1, 0)
 	return kvs
 }
 
 // URLTop10Reduce is the reduce function in the second round.
-func URLTop10Reduce(key string, values []string) string {
+func URLTop10ReduceV1(key string, values []string) string {
 	sort.Strings(values)
 	var result string
 	for _, val := range values {
-		if atomic.LoadInt64(&count) != 10 {
+		if atomic.LoadInt64(&countV1) != 10 {
 			result += fmt.Sprintf("%s: %s\n", val, key)
-			atomic.AddInt64(&count, 1)
+			atomic.AddInt64(&countV1, 1)
 		}
 	}
 	return result
